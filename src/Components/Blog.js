f54615348baa3ecc57e83ee5c08499efd3d99cde
Al Shaharia <ifty682002@gmail.com>
2022-11-17 21:19:05 +0600

    blog demo is done

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -11,7 +11,44 @@ const Blog = () => {
         Check what's popular on Freepik and make your project look trendy and
         professional.
       </p>
-      <BlogCart />
+      <div className="m-10 grid grid-cols-3 gap-4">
+        <BlogCart />
+        <div className="card card-compact bg-base-100 shadow-xl">
+          <figure>
+            <img
+              className="w-full"
+              src="https://placeimg.com/400/225/arch"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">View More..</button>
+            </div>
+          </div>
+        </div>
+        <div className="card card-compact bg-base-100 shadow-xl">
+          <figure>
+            <img
+              className="w-full"
+              src="https://placeimg.com/400/225/arch"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">View More..</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="text-center">
+        <button className="btn btn-outline btn-info">See More Update</button>
+      </div>
     </div>
   );
 };
--- src/Components/BlogCart.js
+++ src/Components/BlogCart.js
@@ -3,7 +3,22 @@ import React from "react";
 const BlogCart = () => {
   return (
     <div>
-      <h2 className="mt-10">Blog Cart</h2>
+      <div className="card card-compact bg-base-100 shadow-xl">
+        <figure>
+          <img
+            className="w-full"
+            src="https://placeimg.com/400/225/arch"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
