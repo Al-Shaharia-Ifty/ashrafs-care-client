5d2cdefd87751ad8f88635480423d16538f0291b
Al Shaharia <ifty682002@gmail.com>
2023-06-21 17:52:17 +0600

    privacy policy page are temporary done

--- src/Pages/Policy.jsx
+++ src/Pages/Policy.jsx
@@ -4,9 +4,162 @@ const Policy = () => {
   return (
     <div>
       <div className="pt-20 bg-green-800"></div>
-      <div>
-        <h2>Policy</h2>
+      <div className="my-10 mx-16 2xl:mx-40 duration-500">
+        <h2 className="text-center text-2xl md:text-3xl lg:text-4xl xl:text-5xl  font-bold">
+          Our Privacy Policy
+        </h2>
+        <ol className="list-decimal">
+          <li className="underline underline-offset-1 mt-5 text-2xl">
+            Purpose
+          </li>
+          <p className="text-justify text-xl font-extralight text-gray-500">
+            The purpose of this policy is to establish guidelines and standards
+            for the management and operation of our marketing organization. It
+            aims to ensure professionalism, ethical behavior and high-quality
+            service delivery to our clients.
+          </p>
+          <li className="underline underline-offset-1 mt-5 text-2xl">
+            Client Engagement
+          </li>
+          <ol className="list-decimal text-justify text-xl font-extralight text-gray-500">
+            <li>
+              We strive to understand our clients' goals, objectives and target
+              audience to deliver effective marketing strategies and campaigns.
+            </li>
+            <li>
+              We will maintain open and transparent communication with our
+              clients throughout the project, providing regular updates and
+              seeking their input and feedback.
+            </li>
+            <li>
+              We will respect client privacy and ensure the security of their
+              sensitive information.
+            </li>
+          </ol>
+          <li className="underline underline-offset-1 mt-5 text-2xl">
+            Professionalism
+          </li>
+          <ol className="list-decimal text-justify text-xl font-extralight text-gray-500">
+            <li>
+              Our team members will maintain a high level of professionalism in
+              all interactions, both internally and externally.
+            </li>
+            <li>
+              We will treat clients, colleagues and partners with respect,
+              courtesy and fairness.
+            </li>
+            <li>
+              We will meet deadlines and deliver the highest quality work to
+              meet or exceed client expectations.
+            </li>
+          </ol>
+
+          <li className="underline underline-offset-1 mt-5 text-2xl">
+            Professionalism
+          </li>
+          <ol className="list-decimal text-justify text-xl font-extralight text-gray-500">
+            <li>
+              We will conduct our business operations ethically in compliance
+              with all applicable laws, regulations and industry standards.
+            </li>
+            <li>
+              We will not engage in any fraudulent or unethical practices,
+              including false advertising, plagiarism or unauthorized use of
+              intellectual property.
+            </li>
+            <li>
+              We will respect individuals' privacy rights and comply with data
+              protection regulations.
+            </li>
+          </ol>
+
+          <li className="underline underline-offset-1 mt-5 text-2xl">
+            Team collaboration
+          </li>
+          <ol className="list-decimal text-justify text-xl font-extralight text-gray-500">
+            <li>
+              We will foster a collaborative and inclusive work environment,
+              promoting teamwork and open communication among team members.
+            </li>
+            <li>
+              We will encourage professional development and provide
+              opportunities for skill development and growth.
+            </li>
+            <li>
+              We will recognize and appreciate the contributions and
+              achievements of our team members.
+            </li>
+          </ol>
+
+          <li className="underline underline-offset-1 mt-5 text-2xl">
+            Privacy and Security
+          </li>
+          <ol className="list-decimal text-justify text-xl font-extralight text-gray-500">
+            <li>
+              We will treat all client information and data with utmost
+              confidentiality and take necessary measures to ensure its
+              security.
+            </li>
+            <li>
+              We will implement appropriate security protocols and technologies
+              to protect Client Data from unauthorized access, loss or
+              disclosure.
+            </li>
+          </ol>
+
+          <li className="underline underline-offset-1 mt-5 text-2xl">
+            Intellectual Property
+          </li>
+          <ol className="list-decimal text-justify text-xl font-extralight text-gray-500">
+            <li>
+              We respect intellectual property rights and will not infringe the
+              copyrights, trademarks or patents of others.
+            </li>
+            <li>
+              We will respect client ownership of deliverables and intellectual
+              property developed during our engagement.
+            </li>
+          </ol>
+
+          <li className="underline underline-offset-1 mt-5 text-2xl">
+            Performance measurement
+          </li>
+          <ol className="list-decimal text-justify text-xl font-extralight text-gray-500">
+            <li>
+              We will establish performance metrics and key performance
+              indicators (KPIs) to evaluate the effectiveness and efficiency of
+              our marketing campaigns.
+            </li>
+            <li>
+              We will continuously monitor and analyze campaign performance to
+              make data-driven decisions to optimize results.
+            </li>
+            <li>
+              We will conduct regular client satisfaction surveys and seek
+              feedback to identify areas for improvement.
+            </li>
+          </ol>
+
+          <li className="underline underline-offset-1 mt-5 text-2xl">
+            Consent
+          </li>
+          <ol className="list-decimal text-justify text-xl font-extralight text-gray-500">
+            <li>
+              All team members are expected to familiarize themselves with this
+              policy and abide by its provisions
+            </li>
+            <li>
+              Non-compliance with this policy may result in disciplinary action,
+              up to and including termination of employment or engagement.
+            </li>
+          </ol>
+        </ol>
       </div>
+      <p className="my-10 text-2xl text-justify mx-10 2xl:mx-40 duration-500 ">
+        Also maintain Facebook's Community Standards and Advertising Policy. We
+        are not responsible for rejecting, restricting and disabling any content
+        posted outside of Facebook's policy.
+      </p>
     </div>
   );
 };
